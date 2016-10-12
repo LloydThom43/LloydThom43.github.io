66ee9c6dedb677d060d38b2e84fa08e3b019bbe2
Lloyd Davies <lloydthom43@hotmail.co.uk>
2016-10-12 12:54:52 +0100

    Done a few changes and added an extra page.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,13 @@
 console.log('This would be the main JS file.');
+
+$( document ).ready(function() {
+   
+    $('#Blog-Link').click(function(){
+        alert("Not Available yet!");
+    });
+         
+    $('#Projects-Link').click(function(){
+        alert("Not Available yet!");
+    });
+        
+});
